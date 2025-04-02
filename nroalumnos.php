9c0bd33f78e53afd619295fd30335b928479548f
Emerson <emeralvarado1983@gmail.com>
2025-04-02 16:26:56 -0400

    Se modifico la tabla y se cambio el grafico

--- nroalumnos.php
+++ nroalumnos.php
@@ -3,87 +3,84 @@ $datos = [
     [
         "municipio" => "ACARI",
         "año" => 2022,
-        "guarderia_publica_tc" => 1220,
-        "guarderia_afiliada_tc" => 770,
-        "cuidado_tc" => 1990,
-        "guarderia_publica_tp" => 1005,
-        "guarderia_afiliada_tp" => 600,
-        "cuidado_tp" => 1605,
-        "preescolar_publico_tc" => 2283,
-        "preescolar_afiliado_tc" => 1877,
-        "preescolar_tc" => 4160,
-        "preescolar_publico_tp" => 1600,
-        "preescolar_afiliado_tp" => 900,
-        "preescolar_tp" => 2500,
-        "primaria_tp_inicial" => 5210,
-        "primaria_tp_final" => 5100,
-        "serie_urbana_inicial" => 450,
-        "serie_rural_inicial" => 200,
-        "serie_urbana_final" => 420,
-        "serie_rural_final" => 175,
-        "tiempo_integral" => 4300,
-        "educacion_especial" => 430,
-        "servicio_educacion_especial_aee" => 80,
-        "evaluacion_proceso" => 100,
-        "educacion_indigena_quilombo" => 30,
-        "promedio_total" => 26750
+        "creche_em_tempo_integral_publica" => 1220,
+        "creche_em_tempo_integral_conveniada" => 770,
+        "creche_tempo_integral" => 1990,
+        "creche_em_tempo_parcial_publica" => 1005,
+        "creche_em_tempo_parcial_conveniada" => 600,
+        "creche_parcial" => 1605,
+        "pre_escola_em_tempo_integral_publica" => 2283,
+        "pre_escola_em_tempo_integral_conveniada" => 1877,
+        "pre_escola_em_tempo_integral" => 4160,
+        "pre_escola_em_tempo_parcial_publica" => 1600,
+        "pre_escola_em_tempo_parcial_conveniada" => 900,
+        "pre_escola_parcial" => 2500,
+        "ensino_fundamental_em_tempo_parcial_anos_iniciais" => 5210,
+        "ensino_fundamental_em_tempo_parcial_anos_finais" => 5100,
+        "series_iniciais_urbanas" => 450,
+        "series_iniciais_rurais" => 200,
+        "series_finais_urbanas" => 420,
+        "series_finais_rurais" => 175,
+        "tempo_integral" => 4300,
+        "educacao_especial" => 430,
+        "atendimiento_educacao_especial_aee" => 80,
+        "avaliacao_no_processo" => 100,
+        "educ_indigena_quilombo" => 30
     ],
     [
-        "municipio" => "BARAUNA",
-        "año" => 2022,
-        "guarderia_publica_tc" => 1300,
-        "guarderia_afiliada_tc" => 800,
-        "cuidado_tc" => 2100,
-        "guarderia_publica_tp" => 1100,
-        "guarderia_afiliada_tp" => 650,
-        "cuidado_tp" => 1750,
-        "preescolar_publico_tc" => 2400,
-        "preescolar_afiliado_tc" => 1900,
-        "preescolar_tc" => 4300,
-        "preescolar_publico_tp" => 1650,
-        "preescolar_afiliado_tp" => 950,
-        "preescolar_tp" => 2600,
-        "primaria_tp_inicial" => 5300,
-        "primaria_tp_final" => 5200,
-        "serie_urbana_inicial" => 460,
-        "serie_rural_inicial" => 210,
-        "serie_urbana_final" => 430,
-        "serie_rural_final" => 180,
-        "tiempo_integral" => 4400,
-        "educacion_especial" => 440,
-        "servicio_educacion_especial_aee" => 85,
-        "evaluacion_proceso" => 110,
-        "educacion_indigena_quilombo" => 35,
-        "promedio_total" => 27600
-    ],
-    [
-       "municipio" => "BARRA DE SANTIAGO",
+        "municipio" => "ACARI",
         "año" => 2023,
-        "guarderia_publica_tc" => 1400,
-        "guarderia_afiliada_tc" => 850,
-        "cuidado_tc" => 2250,
-        "guarderia_publica_tp" => 1200,
-        "guarderia_afiliada_tp" => 700,
-        "cuidado_tp" => 1900,
-        "preescolar_publico_tc" => 2500,
-        "preescolar_afiliado_tc" => 2000,
-        "preescolar_tc" => 4500,
-        "preescolar_publico_tp" => 1700,
-        "preescolar_afiliado_tp" => 1000,
-        "preescolar_tp" => 2700,
-        "primaria_tp_inicial" => 5400,
-        "primaria_tp_final" => 5300,
-        "serie_urbana_inicial" => 470,
-        "serie_rural_inicial" => 220,
-        "serie_urbana_final" => 440,
-        "serie_rural_final" => 185,
-        "tiempo_integral" => 4500,
-        "educacion_especial" => 450,
-        "servicio_educacion_especial_aee" => 90,
-        "evaluacion_proceso" => 120,
-        "educacion_indigena_quilombo" => 40,
-        "promedio_total" => 28565
+        "creche_em_tempo_integral_publica" => 1300,
+        "creche_em_tempo_integral_conveniada" => 800,
+        "creche_tempo_integral" => 2100,
+        "creche_em_tempo_parcial_publica" => 1100,
+        "creche_em_tempo_parcial_conveniada" => 650,
+        "creche_parcial" => 1750,
+        "pre_escola_em_tempo_integral_publica" => 2400,
+        "pre_escola_em_tempo_integral_conveniada" => 1900,
+        "pre_escola_em_tempo_integral" => 4300,
+        "pre_escola_em_tempo_parcial_publica" => 1650,
+        "pre_escola_em_tempo_parcial_conveniada" => 950,
+        "pre_escola_parcial" => 2600,
+        "ensino_fundamental_em_tempo_parcial_anos_iniciais" => 5300,
+        "ensino_fundamental_em_tempo_parcial_anos_finais" => 5200,
+        "series_iniciais_urbanas" => 460,
+        "series_iniciais_rurais" => 210,
+        "series_finais_urbanas" => 430,
+        "series_finais_rurais" => 180,
+        "tempo_integral" => 4400,
+        "educacao_especial" => 440,
+        "atendimiento_educacao_especial_aee" => 85,
+        "avaliacao_no_processo" => 110,
+        "educ_indigena_quilombo" => 35
     ],
+    [
+        "municipio" => "ACARI",
+        "año" => 2024,
+        "creche_em_tempo_integral_publica" => 1400,
+        "creche_em_tempo_integral_conveniada" => 850,
+        "creche_tempo_integral" => 2250,
+        "creche_em_tempo_parcial_publica" => 1200,
+        "creche_em_tempo_parcial_conveniada" => 700,
+        "creche_parcial" => 1900,
+        "pre_escola_em_tempo_integral_publica" => 2500,
+        "pre_escola_em_tempo_integral_conveniada" => 2000,
+        "pre_escola_em_tempo_integral" => 4500,
+        "pre_escola_em_tempo_parcial_publica" => 1700,
+        "pre_escola_em_tempo_parcial_conveniada" => 1000,
+        "pre_escola_parcial" => 2700,
+        "ensino_fundamental_em_tempo_parcial_anos_iniciais" => 5400,
+        "ensino_fundamental_em_tempo_parcial_anos_finais" => 5300,
+        "series_iniciais_urbanas" => 470,
+        "series_iniciais_rurais" => 220,
+        "series_finais_urbanas" => 440,
+        "series_finais_rurais" => 185,
+        "tempo_integral" => 4500,
+        "educacao_especial" => 450,
+        "atendimiento_educacao_especial_aee" => 90,
+        "avaliacao_no_processo" => 120,
+        "educ_indigena_quilombo" => 40
+    ]
 ];
 $años = array_unique(array_column($datos, 'año'));
 ?>
@@ -139,128 +136,136 @@ $años = array_unique(array_column($datos, 'año'));
             background-color: #f2f2f2;
         }
 
-        canvas {
-            max-width: 100%;
+        div#graficos-contenedor {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
         }
     </style>
 </head>
 <body>
 <div class="contenedor">
+    <h2 style="text-align: center; color: #4CAF50;">
+        Municipio: <?= $datos[0]['municipio'] ?>
+    </h2>
+
     <div class="contenedor-tabla">
         <table>
             <thead>
                 <tr>
-                    <th>Municipios</th>
                     <th>Años</th>
-                    <th>Guarderia Pública TC</th>
-                    <th>Guarderia Afiliada TC</th>
-                    <th>Cuidado TC</th>
-                    <th>Guarderia Pública TP</th>
-                    <th>Guarderia Afiliada TP</th>
-                    <th>Cuidado TP</th>
-                    <th>Preescolar Público TC</th>
-                    <th>Preescolar Afiliado TC</th>
-                    <th>Preescolar TC</th>
-                    <th>Preescolar Público TP</th>
-                    <th>Preescolar Afiliado TP</th>
-                    <th>Preescolar TP</th>
-                    <th>Primaria TP Inicial</th>
-                    <th>Primaria TP Final</th>
-                    <th>Serie Urbana Inicial</th>
-                    <th>Serie Rural Inicial</th>
-                    <th>Serie Urbana Final</th>
-                    <th>Serie Rural Final</th>
-                    <th>Tiempo Integral</th>
-                    <th>Educación Especial</th>
-                    <th>Servicio Educación Especial AEE</th>
-                    <th>Evaluación Proceso</th>
-                    <th>Educación Indígena / Quilombo</th>
-                    <th>Promedio Total</th>
+                    <th>Creche em tempo integral pública</th>
+                    <th>Creche em tempo integral conveniada</th>
+                    <th>CRECHE TEMPO INTEGRAL</th>
+                    <th>Creche em tempo parcial pública</th>
+                    <th>Creche em tempo parcial pública</th>
+                    <th>CRECHE PARCIAL</th>
+                    <th>Pré-Escola em tempo integral pública</th>
+                    <th>Pré-Escola em tempo integral conveniada</th>
+                    <th>PRÉ-ESCOLA TEMPO INTEGRAL</th>
+                    <th>Pré-Escola em tempo parcial pública</th>
+                    <th>Pré-Escola em tempo parcial conveniada</th>
+                    <th>PRÉ-ESCOLA PARCIAL</th>
+                    <th>Ensino Fundamental em tempo parcial - anos INICIAIS</th>
+                    <th>Ensino Fundamental em tempo parcial - anos FINAIS</th>
+                    <th>SÉRIES INICIAIS URBANAS</th>
+                    <th>SÉRIES INICIAIS RURAIS</th>
+                    <th>SÉRIES FINAIS URBANAS</th>
+                    <th>SÉRIES FINAIS RURAIS</th>
+                    <th>TEMPO INTEGRAL</th>
+                    <th>EDUCAÇÃO ESPECIAL</th>
+                    <th>ATENDIMENTO EDUCAÇÃO ESPECIAL AEE</th>
+                    <th>AVALIAÇÃO NO PROCESSO</th>
+                    <th>EDUC. INDÍGENA / QUILOMBO</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($datos as $dato) : ?>
                     <tr>
-                        <td><?= $dato['municipio'] ?></td>
                         <td><?= $dato['año'] ?></td>
-                        <td><?= $dato['guarderia_publica_tc'] ?></td>
-                        <td><?= $dato['guarderia_afiliada_tc'] ?></td>
-                        <td><?= $dato['cuidado_tc'] ?></td>
-                        <td><?= $dato['guarderia_publica_tp'] ?></td>
-                        <td><?= $dato['guarderia_afiliada_tp'] ?></td>
-                        <td><?= $dato['cuidado_tp'] ?></td>
-                        <td><?= $dato['preescolar_publico_tc'] ?></td>
-                        <td><?= $dato['preescolar_afiliado_tc'] ?></td>
-                        <td><?= $dato['preescolar_tc'] ?></td>
-                        <td><?= $dato['preescolar_publico_tp'] ?></td>
-                        <td><?= $dato['preescolar_afiliado_tp'] ?></td>
-                        <td><?= $dato['preescolar_tp'] ?></td>
-                        <td><?= $dato['primaria_tp_inicial'] ?></td>
-                        <td><?= $dato['primaria_tp_final'] ?></td>
-                        <td><?= $dato['serie_urbana_inicial'] ?></td>
-                        <td><?= $dato['serie_rural_inicial'] ?></td>
-                        <td><?= $dato['serie_urbana_final'] ?></td>
-                        <td><?= $dato['serie_rural_final'] ?></td>
-                        <td><?= $dato['tiempo_integral'] ?></td>
-                        <td><?= $dato['educacion_especial'] ?></td>
-                        <td><?= $dato['servicio_educacion_especial_aee'] ?></td>
-                        <td><?= $dato['evaluacion_proceso'] ?></td>
-                        <td><?= $dato['educacion_indigena_quilombo'] ?></td>
-                        <td><?= $dato['promedio_total'] ?></td>
+                        <td><?= $dato['creche_em_tempo_integral_publica'] ?></td>
+                        <td><?= $dato['creche_em_tempo_integral_conveniada'] ?></td>
+                        <td><?= $dato['creche_tempo_integral'] ?></td>
+                        <td><?= $dato['creche_em_tempo_parcial_publica'] ?></td>
+                        <td><?= $dato['creche_em_tempo_parcial_conveniada'] ?></td>
+                        <td><?= $dato['creche_parcial'] ?></td>
+                        <td><?= $dato['pre_escola_em_tempo_integral_publica'] ?></td>
+                        <td><?= $dato['pre_escola_em_tempo_integral_conveniada'] ?></td>
+                        <td><?= $dato['pre_escola_em_tempo_integral'] ?></td>
+                        <td><?= $dato['pre_escola_em_tempo_parcial_publica'] ?></td>
+                        <td><?= $dato['pre_escola_em_tempo_parcial_conveniada'] ?></td>
+                        <td><?= $dato['pre_escola_parcial'] ?></td>
+                        <td><?= $dato['ensino_fundamental_em_tempo_parcial_anos_iniciais'] ?></td>
+                        <td><?= $dato['ensino_fundamental_em_tempo_parcial_anos_finais'] ?></td>
+                        <td><?= $dato['series_iniciais_urbanas'] ?></td>
+                        <td><?= $dato['series_iniciais_rurais'] ?></td>
+                        <td><?= $dato['series_finais_urbanas'] ?></td>
+                        <td><?= $dato['series_finais_rurais'] ?></td>
+                        <td><?= $dato['tempo_integral'] ?></td>
+                        <td><?= $dato['educacao_especial'] ?></td>
+                        <td><?= $dato['atendimiento_educacao_especial_aee'] ?></td>
+                        <td><?= $dato['avaliacao_no_processo'] ?></td>
+                        <td><?= $dato['educ_indigena_quilombo'] ?></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
     </div>
-    <label for="yearSelect">Selecciona un año:</label>
-    <select id="yearSelect">
-        <?php foreach ($años as $año) : ?>
-            <option value="<?= $año ?>"><?= $año ?></option>
-        <?php endforeach; ?>
-    </select>
-    <canvas id="grafico"></canvas>
+
+    <div id="graficos-contenedor"></div>
 </div>
 
 <script>
     const datos = <?php echo json_encode($datos); ?>;
-    const ctx = document.getElementById("grafico").getContext("2d");
-    const yearSelect = document.getElementById("yearSelect");
+    const ctx = document.getElementById("graficos-contenedor");
 
-    // Asegurar que el select tenga 2022 por defecto
-    const añoPorDefecto = 2022;
-    yearSelect.value = añoPorDefecto;
+    const colores = [
+        "#FF6384", "#36A2EB", "#FFCE56", "#4BC0C0", "#9966FF", "#FF9F40",
+        "#FFCD56", "#C9CBCF", "#32CD32", "#8A2BE2", "#00CED1", "#DC143C",
+        "#7FFF00", "#FFD700", "#6495ED", "#A52A2A", "#6B8E23", "#FF4500",
+        "#2E8B57", "#B22222", "#D2691E", "#483D8B"
+    ];
 
-    function filtrarDatosPorAño(año) {
-        return datos.filter(d => d.año == año);
+    function crearGraficoDoughnut(datosFiltrados, idCanvas) {
+        if (!datosFiltrados) return;
+
+        // Excluir "año" y "promedio_total"
+        const labels = Object.keys(datosFiltrados).filter(key => key !== "año" && key !== "municipio");
+        const valores = labels.map(key => datosFiltrados[key]);
+
+        new Chart(document.getElementById(idCanvas).getContext("2d"), {
+            type: "doughnut",
+            data: {
+                labels: labels,
+                datasets: [{
+                    data: valores,
+                    backgroundColor: colores.slice(0, labels.length),
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: { position: "right" }
+                }
+            }
+        });
     }
 
-    function crearDataset(datosFiltrados) {
-        return {
-            labels: datosFiltrados.map(d => d.municipio),
-            datasets: [{
-                label: "Promedio Total",
-                data: datosFiltrados.map(d => d.promedio_total),
-                backgroundColor: "rgba(255, 99, 132, 0.5)",
-                borderColor: "rgba(255, 99, 132, 1)",
-                borderWidth: 1
-            }]
-        };
-    }
+    // Generar los gráficos para cada año
+    datos.forEach((dato) => {
+        const año = dato.año;
+        const canvasId = `grafico-${año}`;
 
-    // Filtrar los datos por el año predeterminado antes de inicializar el gráfico
-    let datosFiltrados = filtrarDatosPorAño(añoPorDefecto);
+        // Crear el título y el canvas para el gráfico
+        const section = document.createElement("div");
+        section.innerHTML = `<h3 style="text-align: center;">Datos del Año ${año}</h3>
+                             <canvas id="${canvasId}" style="width: 585px; height: 585px; margin: auto;"></canvas>`;
 
-    const chart = new Chart(ctx, {
-        type: "bar",
-        data: crearDataset(datosFiltrados),
-        options: { responsive: true, scales: { y: { beginAtZero: true } } }
-    });
-    
-    // Evento para actualizar el gráfico al cambiar el año
-    yearSelect.addEventListener("change", function () {
-        datosFiltrados = filtrarDatosPorAño(this.value);
-        chart.data = crearDataset(datosFiltrados);
-        chart.update();
+        document.getElementById("graficos-contenedor").appendChild(section);
+
+        // Crear el gráfico
+        setTimeout(() => crearGraficoDoughnut(dato, canvasId), 100);
+
     });
 </script>
 </body>
